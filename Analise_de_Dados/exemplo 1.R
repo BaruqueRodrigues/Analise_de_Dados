4096cfc9a3b7fb214ab999d5a46d9884d933f3ac
BaruqueRodrigues <81967584+BaruqueRodrigues@users.noreply.github.com>
2021-04-05 10:05:28 -0300

    Add files via upload

---
+++ Analise_de_Dados/exemplo 1.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(dplyr)
+
+melhor_time_br<-"Flamengo"
+campeao_87<-"Flamengo"
+
+graf <- diamonds%>%filter(cut=="Ideal")%>%
+  ggplot(aes(price))+
+    geom_bar()+
+  labs(y=NULL, x= "Preço", title = "Distribuição do Preço dos Diamantes com cortes Ideais",
+       caption = "Fonte:")
+  
+str(melhor_time_br)
+str(graf)
+
